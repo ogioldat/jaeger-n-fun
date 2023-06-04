12512cfd4481caff5d651bdb4b637185bdcdc8f5
ogioldat <tomasz.ogiolda00@gmail.com>
2023-06-04 10:56:09 +0200

    fixup! prepare working demo

--- scripts/jaeger-ui.sh
+++ scripts/jaeger-ui.sh
@@ -1,5 +1,5 @@
 # !/bin/zsh
 
-kubectl port-forward -n jaeger-n-fun $(kubectl get pods -n jaeger-n-fun -l=app="jaeger" -o name) 16686:16686
-
 open http://localhost:16686/
+
+kubectl port-forward -n jaeger-n-fun $(kubectl get pods -n jaeger-n-fun -l=app="jaeger" -o name) 16686:16686
